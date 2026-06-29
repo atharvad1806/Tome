9ef17b33efda0bdd32c3642baf83d1f678eae9ee
Atharva Dhupkar <atharvadhupkar18@gmail.com>
2026-06-29 12:32:09 +0530

    sample ebook created

---
+++ src/create_test_epub.py
@@ -0,0 +1,22 @@
+from ebooklib import epub
+
+book = epub.EpubBook()
+book.set_title('Test Book')
+book.set_language('en')
+book.add_author('AD')
+
+c1 = epub.EpubHtml(title="Chapter 1", file_name = 'ch1.xhtml', lang='en')
+c1.content = c1.content = '<h1>Chapter 1: The Beginning</h1><p>' + ('Once upon a time there was a small dragon named Ember who lived in a cave. ' * 40) + '</p>'
+
+c2 = epub.EpubHtml(title="Chapter 2", file_name = 'ch2.xhtml', lang='en')
+c2.content = c2.content = '<h1>Chapter 1: The Journey</h1><p>' + ('Ember decided to leave the cave and explore the world beyond the mountains. ' * 40) + '</p>'
+
+book.add_item(c1)
+book.add_item(c2)
+book.toc = (c1, c2)
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+book.spine = ['nav', c1, c2]
+
+epub.write_epub('sample_books/test.epub', book)
+print('created test epub')
